b1c7d6dac88965685ed50f9f05ae3476d07221bb
Juan Sesan <juansesan@discover.com>
2023-09-29 00:52:15 -0300

    add database phase 1

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `patient-registration`;
+USE `patient-registration`;
+DROP TABLE IF EXISTS `patients`;
+
+CREATE TABLE `patients` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  `phone` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(65) NOT NULL,
+  `image` BLOB NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
